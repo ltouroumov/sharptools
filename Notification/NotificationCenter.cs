ad1179e97acfb42379b88c20668c59b03d99e360
Jeremy David <jd@jeremydavid.ch>
2013-05-28 14:25:11 +0200

    New notification center

---
+++ Notification/NotificationCenter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTools.Notification
+{
+    public class NotificationCenter
+    {
+        public static readonly NotificationCenter DefaultCenter = new NotificationCenter();
+
+        private IDictionary<string, NotificationHandler> Handlers;
+
+        public delegate void NotificationEventHandler(object sender, Notification notification);
+        public event NotificationEventHandler OnNotify;
+
+        public NotificationCenter()
+        {
+            Handlers = new Dictionary<string, NotificationHandler>();
+        }
+
+        public NotificationHandler this[string key]
+        {
+            get { return FetchHandler(key); }
+        }
+
+        public NotificationHandler FetchHandler(string key)
+        {
+            NotificationHandler handler = null;
+
+            if (!Handlers.TryGetValue(key, out handler)) {
+                handler = Handlers[key] = new NotificationHandler(this, key);
+            }
+
+            return handler;
+        }
+
+        internal void PostNotification(object sender, Notification notif)
+        {
+            if (OnNotify != null) {
+                OnNotify(sender, notif);
+            }
+        }
+    }
+
+    public class Notification
+    {
+        public string Name { get; internal set; }
+
+        public object Parameter { get; internal set; }
+    }
+
+    private class NotificationHandler
+    {
+        private NotificationCenter center;
+        public string Name { get; private set; }
+
+        public delegate void NotificationEventHandler(object sender, Notification notification);
+        public event NotificationEventHandler OnNotify;
+
+        public NotificationHandler(NotificationCenter center, string name)
+        {
+            this.Name = name;
+            this.center = center;
+        }
+
+        public void Observe(NotificationEventHandler handler)
+        {
+            OnNotify += handler;
+        }
+
+        public void Post(object sender, object parameter = null)
+        {
+            var notif = new Notification() {
+                Name = Name,
+                Parameter = parameter
+            };
+
+            if (OnNotify != null) {
+                OnNotify(sender, notif);
+            }
+        }
+    }
+}
