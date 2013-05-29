94bcbd73a366fc32b6955c8e0612ffcae3e19664
Jeremy David <ltouroumov@gmail.com>
2013-05-29 09:24:45 +0200

    Project merge mixup

---
+++ Config/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using SharpTools.Xml;
+using SharpTools.Utils.Collections;
+
+namespace SharpTools.Config
+{
+    class Config : Dictionary<string, string>
+    {
+        public static Config Load(string path)
+        {
+            var reader = XmlReader.Create(path);
+            reader.ReadUntil(xml => xml.NodeType == XmlNodeType.Element);
+            var config = new Config();
+
+            reader.ObjectNodeList("Parameter",
+                (x) => new Tuple() { Key = x.GetAttribute("Name") },
+                (x, node) => x.ReadText().Bind(val => node.Val = val))
+                .Bind(list => {
+                    list.ForEach(t => config.Add(t.Key, t.Val));
+                });
+
+            return config;
+        }
+
+        protected class Tuple
+        {
+            public string Key { get; set; }
+            public string Val { get; set; }
+        }
+    }
+}
