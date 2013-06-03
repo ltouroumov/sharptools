582f733de8204460084cd25624a6111516a12c5c
Jeremy David <ltouroumov@gmail.com>
2013-06-03 11:25:08 +0200

    Added some unit tests

---
+++ SharpTests/FunctionalCompositionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTools.Functional;
+
+namespace SharpTests
+{
+    [TestClass]
+    public class FunctionalCompositionTest
+    {
+        [TestMethod]
+        public void SimpleCompose()
+        {
+            Func<int, int> func1 = (a) => a + 1;
+            Func<int, int> func2 = (a) => a * 2;
+
+            var func3 = func1.Compose(func2);
+            var func4 = func2.Compose(func1);
+
+            Assert.AreEqual((10 * 2) + 1, func3(10));
+            Assert.AreEqual((10 + 1) * 2, func4(10));
+        }
+
+        [TestMethod]
+        public void SimpleAndThen()
+        {
+            Func<int, int> func1 = (a) => a + 1;
+            Func<int, int> func2 = (a) => a * 2;
+
+            var func3 = func1.AndThen(func2);
+            var func4 = func2.AndThen(func1);
+
+            Assert.AreEqual((10 + 1) * 2, func3(10));
+            Assert.AreEqual((10 * 2) + 1, func4(10));
+        }
+    }
+}
---
+++ SharpTests/FunctionalOptionTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTools.Functional.Option;
+
+namespace SharpTests
+{
+    [TestClass]
+    public class FunctionalOptionTest
+    {
+        [TestMethod]
+        public void SomeAndSome()
+        {
+            var opt1 = Some.New("Hello");
+            var opt2 = Some.New("World");
+
+            var opt3 = opt1.Bind(o1 =>
+                       opt2.Bind(o2 => Some.New(o1 + o2)));
+
+            Assert.IsInstanceOfType(opt3, typeof(Some<string>));
+            Assert.AreEqual("HelloWorld", (opt3 as Some<string>).Value);
+        }
+
+        [TestMethod]
+        public void SomeAndNothing()
+        {
+            var opt1 = Some.New("Hello");
+            var opt2 = Nothing.New<string>();
+
+            var opt3 = opt1.Bind(o1 =>
+                       opt2.Bind(o2 => {
+                           Assert.Fail();
+                           return Some.New(o1+o2);
+                       }));
+
+            Assert.IsInstanceOfType(opt3, typeof(Nothing<string>));
+        }
+
+        [TestMethod]
+        public void SomeAndNothingWithAction()
+        {
+            var opt1 = Some.New("Hello");
+            var opt2 = Nothing.New<string>();
+
+            var opt3 = opt1.Bind(o1 =>
+                       opt2.Bind(o2 => {
+                           Assert.Fail();
+                       }));
+
+            Assert.IsInstanceOfType(opt3, typeof(Nothing<string>));
+        }
+
+        [TestMethod]
+        public void ValueConversion()
+        {
+            var some = Some.New("HelloWorld");
+            var nothing = Nothing.New<string>();
+
+            Assert.AreEqual("HelloWorld", some.ToValue());
+            Assert.AreEqual(null, nothing.ToValue());
+        }
+
+        [TestMethod]
+        public void SomeAndSomeWithAction()
+        {
+            var opt1 = Some.New("Hello");
+            var opt2 = Some.New("World");
+
+            var opt3 = opt1.Bind(o1 => 
+                       opt2.Bind(o2 => {
+                           Assert.IsTrue(true);
+                       }));
+        }
+
+        [TestMethod]
+        public void NothingWhenNothing()
+        {
+            var opt1 = Nothing.New<bool>();
+
+            opt1.WhenNothing(() => {
+                Assert.IsTrue(true);
+            });
+        }
+
+        [TestMethod]
+        public void SomeWhenNothing()
+        {
+            var opt1 = Some.New(true);
+
+            opt1.WhenNothing(() =>
+            {
+                Assert.Fail();
+            });
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void DoubleBind()
+        {
+            var opt1 = Some.New("Hello");
+            var opt2 = Some.New("World");
+
+            var opt3 = opt1.BindWith(opt2, (o1, o2) =>
+                            Some.New(o1 + o2));
+
+            Assert.IsInstanceOfType(opt3, typeof(Some<string>));
+            Assert.AreEqual("HelloWorld", (opt3 as Some<string>).Value);
+        }
+
+    }
+}
