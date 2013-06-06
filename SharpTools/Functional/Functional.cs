58de5f04b50804df5338662c5c2f63a21d783e42
Jeremy David <ltouroumov@gmail.com>
2013-06-06 11:46:18 +0200

    Updated docs to be real docs

--- SharpTools/Functional/Functional.cs
+++ SharpTools/Functional/Functional.cs
@@ -4,51 +4,99 @@ namespace SharpTools.Functional
 {
     public static class Function
     {
-        // <summary>
-        // Provides an identity operation for functions
-        // </summary>
+        /// <summary>
+        /// Provides an identity function which returns its first argument
+        /// </summary>
+        /// <typeparam name="A">Return and input type</typeparam>
+        /// <returns>Identity function</returns>
         public static Func<A, A> Identity<A>()
         {
             return a => a;
         }
 
-        // <summary>
-        // Create a function that returns a specific value
-        // </summary>
+        /// <summary>
+        /// Creates a function that will always return the same value when invoked
+        /// </summary>
+        /// <typeparam name="A">Return type</typeparam>
+        /// <param name="value">Value to return</param>
+        /// <returns>A function</returns>
         public static Func<A> Unit<A>(A value)
         {
             return () => value;
         }
+
+        /// <summary>
+        /// Creates a function that will always return the same value when invoked with any parameter.
+        /// WARNING: This is a dangerous function.
+        /// </summary>
+        /// <typeparam name="A">Return and input type</typeparam>
+        /// <param name="value">Value to return</param>
+        /// <returns>A function</returns>
+        public static Func<A, A> Unit<A>(A value)
+        {
+            return a => value;
+        }
     }
 
-    // <summary>
-    // Allows to compose two functions together
-    // </summary>
+    /// <summary>
+    /// Various functions to group other functions together
+    /// </summary>
     public static class FuncCompositionExtensions
     {
-        public static Func<A, C> Compose<A, B, C>(this Func<B, C> func1, Func<A, B> func2)
+        /// <summary>
+        /// Will compose f with g as <code>x -> f(g(x))</code>.
+        /// </summary>
+        /// <param name="f">First function</param>
+        /// <param name="g">Second function</param>
+        /// <returns>Composed function</returns>
+        public static Func<A, C> Compose<A, B, C>(this Func<B, C> f, Func<A, B> g)
         {
-            return (a) => func1(func2(a));
-        }
-        
-        public static Action<B> Compose<A, B>(this Action<A> func1, Func<B, A> func2)
-        {
-            return (a) => func1(func2(a));
-        }
-
-        public static Func<A, C> AndThen<A, B, C>(this Func<A, B> func1, Func<B, C> func2)
-        {
-            return (a) => func2(func1(a));
-        }
-        
-        public static Action<A> AndThen<A, B>(this Func<A, B> func1, Action<B> func2)
-        {
-            return (a) => func2(func1(a));
+            return (a) => f(g(a));
         }
 
-        public static Action AndThen(this Action func1, Action func2)
+
+        /// <summary>
+        /// Will compose f with g as <code>x -> f(g(x))</code>. In which f has no return type.
+        /// </summary>
+        /// <param name="f">First function</param>
+        /// <param name="g">Second function</param>
+        /// <returns>Composed function</returns>
+        public static Action<B> Compose<A, B>(this Action<A> f, Func<B, A> g)
         {
-            return () => { func1(); func2(); };
+            return (a) => f(g(a));
+        }
+
+        /// <summary>
+        /// Will compose f with g as <code>x -> g(f(x))</code>.
+        /// </summary>
+        /// <param name="f">First function</param>
+        /// <param name="g">Second function</param>
+        /// <returns>Composed function</returns>
+        public static Func<A, C> AndThen<A, B, C>(this Func<A, B> f, Func<B, C> g)
+        {
+            return (a) => g(f(a));
+        }
+
+        /// <summary>
+        /// Will compose f with g as <code>x -> g(f(x))</code>. In which g has no return type.
+        /// </summary>
+        /// <param name="f">First function</param>
+        /// <param name="g">Second function</param>
+        /// <returns>Composed function</returns>
+        public static Action<A> AndThen<A, B>(this Func<A, B> f, Action<B> g)
+        {
+            return (a) => g(f(a));
+        }
+
+        /// <summary>
+        /// Will chain two parameterless functions
+        /// </summary>
+        /// <param name="f">First function</param>
+        /// <param name="g">Second function</param>
+        /// <returns>Composed function</returns>
+        public static Action AndThen(this Action f, Action g)
+        {
+            return () => { f(); g(); };
         }
     }
 }
--- SharpTools/Functional/Option.cs
+++ SharpTools/Functional/Option.cs
@@ -2,25 +2,56 @@
 
 namespace SharpTools.Functional.Option
 {
+    /// <summary>
+    /// Base interface for the Option type. Allows other types to act as an option type without extending the base class
+    /// </summary>
+    /// <typeparam name="A">Type of the wrapped value</typeparam>
     public interface IOption<A>
     {
+        /// <summary>
+        /// Converts the Option type to a value.
+        /// </summary>
+        /// <returns>Wrapped value</returns>
+        /// <exception cref="InvalidOperationException" />
         A ToValue();
+
+        /// <summary>
+        /// Binds the Option type.
+        /// When the type is ISome the binder is called and expects an Option as an output.
+        /// When the type is INothing the binder is NOT called and instead a INothing of the new type is returned
+        /// </summary>
+        /// <typeparam name="B">Return type of the binder function</typeparam>
+        /// <param name="binder">Function to be called when the value is ISome</param>
+        /// <returns>New Option type</returns>
         IOption<B> Bind<B>(Func<A, IOption<B>> binder);
     }
 
+    /// <summary>
+    /// Objects that implement this interface are an option type which carry a value
+    /// </summary>
+    /// <typeparam name="A"></typeparam>
     public interface ISome<A> : IOption<A>
     {
+        /// <summary>
+        /// Value wrapped by the option type
+        /// </summary>
         A Value { get; }
     }
 
+
+    /// <summary>
+    /// Objects that implement this interface are an option type which carries no value.
+    /// </summary>
+    /// <typeparam name="A"></typeparam>
     public interface INothing<A> : IOption<A>
     {
 
     }
 
-    // <summary>
-    // Represents a result with the potential for a value
-    // </summary>
+    /// <summary>
+    /// Concrete implementation of the IOption type.
+    /// </summary>
+    /// <typeparam name="A"><see cref="IOption"/></typeparam>
     public abstract class Option<A> : IOption<A>
     {
         public virtual A ToValue()
@@ -33,9 +64,34 @@ namespace SharpTools.Functional.Option
             return this.Bind(binder) as IOption<B>;
         }
 
+        /// <summary>
+        /// Works the same as <see cref="IOption.Bind"/>
+        /// </summary>
+        /// <typeparam name="B">New wrapped value</typeparam>
+        /// <param name="binder">Binding callback</param>
+        /// <returns>New Option type</returns>
         public abstract Option<B> Bind<B>(Func<A, Option<B>> binder);
+
+        /// <summary>
+        /// Binds a void function and returns self.
+        /// </summary>
+        /// <param name="binder">Binding callback</param>
+        /// <returns>self</returns>
         public abstract Option<A> Bind(Action<A> binder);
+
+        /// <summary>
+        /// This method is the reverse of Bind, the binder is invoked when INothing is encountered.
+        /// </summary>
+        /// <param name="binder">Binding callback</param>
+        /// <returns>self</returns>
         public abstract Option<A> WhenNothing(Action binder);
+
+        /// <summary>
+        /// This method is tasked to provide a fallback value in case it encounters a Nothing value.
+        /// </summary>
+        /// <param name="binder">Value provider</param>
+        /// <returns>A value</returns>
+        public abstract Option<A> Fallback(Func<Option<A>> binder);
 
         protected virtual InvalidOperationException MakeException(string method = null)
         {
@@ -51,9 +107,10 @@ namespace SharpTools.Functional.Option
         }
     }
 
-    // <summary>
-    // Represents the presentce of value
-    // </summary>
+    /// <summary>
+    /// Implementation of Option and ISome
+    /// </summary>
+    /// <typeparam name="A">Wrapped value</typeparam>
     public class Some<A> : Option<A>, ISome<A>
     {
         public A Value { get; private set; }
@@ -78,22 +135,28 @@ namespace SharpTools.Functional.Option
             return this;
         }
 
+        // Polymorphism is awesome
         public override Option<A> WhenNothing(Action binder)
+        {
+            return this;
+        }
+
+        public override Option<A> Fallback(Func<Option<A>> binder)
         {
             return this;
         }
     }
 
-    // <summary>
-    // Represents the absence of value
-    // </summary>
+    /// <summary>
+    /// Implementation of Option and INothing
+    /// </summary>
+    /// <typeparam name="A">Wrapped value</typeparam>
     public class Nothing<A> : Option<A>, INothing<A>
     {
         public override A ToValue()
         {
+            // Return the default value for the wrapped type
             return default(A);
-            // This doesnt sound like a good idea
-            // throw new InvalidOperationException("A Nothing monad cannot be converted to a value");
         }
 
         public override Option<B> Bind<B>(Func<A, Option<B>> binder)
@@ -111,8 +174,16 @@ namespace SharpTools.Functional.Option
             binder();
             return this;
         }
+
+        public override Option<A> Fallback(Func<Option<A>> binder)
+        {
+            return binder();
+        }
     }
 
+    /// <summary>
+    /// Static class to allow easy creation of Some type
+    /// </summary>
     public static class Some
     {
         public static Some<A> New<A>(A value)
@@ -121,6 +192,9 @@ namespace SharpTools.Functional.Option
         }
     }
 
+    /// <summary>
+    /// Static class to allow easy creation of Nothing type
+    /// </summary>
     public static class Nothing
     {
         public static Nothing<A> New<A>()
@@ -129,13 +203,31 @@ namespace SharpTools.Functional.Option
         }
     }
 
+
+    /// <summary>
+    /// Static class to allow easy creation of Option types
+    /// </summary>
     public static partial class Option
     {
+        /// <summary>
+        /// Builds a new Option type based on null check
+        /// </summary>
+        /// <typeparam name="A">Wrapped value type</typeparam>
+        /// <param name="value">Value to wrap</param>
+        /// <returns>Wrapped value</returns>
         public static Option<A> New<A>(A value)
         {
             return New(value, val => val != null);
         }
 
+        /// <summary>
+        /// Builds a new Option type based on a discriminator function.
+        /// When the discriminator returns true the value is a Some else the value is Nothing
+        /// </summary>
+        /// <typeparam name="A">Wrapped value type</typeparam>
+        /// <param name="value">Value to wrap</param>
+        /// <param name="discriminator">Discriminator function</param>
+        /// <returns>Wrapped value</returns>
         public static Option<A> New<A>(A value, Func<A, bool> discriminator)
         {
             if (discriminator(value)) {
@@ -145,11 +237,21 @@ namespace SharpTools.Functional.Option
             }
         }
 
+        /// <summary>
+        /// Returns a function that works as an identity to transform its input arguments into Option types.
+        /// </summary>
+        /// <typeparam name="A">Wrappable type</typeparam>
+        /// <returns>Wrapper funtion</returns>
         public static Func<A, Option<A>> Identity<A>()
         {
             return a => Option.New(a);
         }
 
+        /// <summary>
+        /// Returns a function that returns only Nothing.
+        /// </summary>
+        /// <typeparam name="A">Wrappable type</typeparam>
+        /// <returns>Function that returns Nothing</returns>
         public static Func<A, Option<A>> Void<A>()
         {
             return a => Nothing.New<A>();
@@ -158,21 +260,49 @@ namespace SharpTools.Functional.Option
 
     public static class MonadExtensions
     {
+        /// <summary>
+        /// Will transform any value to an Option type
+        /// </summary>
+        /// <typeparam name="A">Value type</typeparam>
+        /// <param name="self">value to wrap</param>
+        /// <returns>Wrapped value</returns>
         public static Option<A> ToMaybe<A>(this A self)
         {
             return Option.New(self);
         }
 
+        /// <summary>
+        /// Attempting to wrap an Option type will return an option type.
+        /// </summary>
+        /// <typeparam name="A"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
         public static Option<A> ToMaybe<A>(this Option<A> self)
         {
             return self;
         }
 
+        /// <summary>
+        /// Double binding function. Allows a couple of Option types to be chained with Bind.
+        /// </summary>
+        /// <typeparam name="A">First wrapped type</typeparam>
+        /// <typeparam name="B">Second wrapped type</typeparam>
+        /// <typeparam name="C">return wrapped type</typeparam>
+        /// <param name="option1">First option type</param>
+        /// <param name="option2">Second option type</param>
+        /// <param name="binder">Combined binder</param>
+        /// <returns>Bound value</returns>
         public static Option<C> BindWith<A, B, C>(this Option<A> option1, Option<B> option2, Func<A, B, Option<C>> binder)
         {
             return option1.Bind(val1 => option2.Bind(val2 => binder(val1, val2)));
         }
 
+        /// <summary>
+        /// Will get the value of Some types and the type's default value for Nothing
+        /// </summary>
+        /// <typeparam name="A">Wrapped value type</typeparam>
+        /// <param name="self">Option value</param>
+        /// <returns>Unwrapped value</returns>
         public static A ToValueOrDefault<A>(this Option<A> self)
         {
             if (self is Nothing<A>) {
@@ -182,6 +312,13 @@ namespace SharpTools.Functional.Option
             }
         }
 
+        /// <summary>
+        /// Will get the value of Some types and invoke the provider for Nothing types
+        /// </summary>
+        /// <typeparam name="A">Wrapped value type</typeparam>
+        /// <param name="self">Option value</param>
+        /// <param name="provider">Fallback callback</param>
+        /// <returns>Unwrapped value</returns>
         public static A ToValueOrDefault<A>(this Option<A> self, Func<A> provider)
         {
             if (self is Nothing<A>) {
