2e56b127c99b6fb9169cf5c38c2b67d6f8968bfd
Jeremy David <ltouroumov@gmail.com>
2013-06-18 10:57:18 +0200

    Added matchable interface

---
+++ SharpTools/Functional/Matchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTools.Functional.Matchable
+{
+    interface IMatchable<A, B>
+    {
+        void Match(Action<A> func1, Action<B> func2);
+    }
+}
